201f74c99bd75db6191f69ab08b2b34dab048f01
E-code804 <erikpfeffer5@gmail.com>
2024-01-25 20:43:20 -0500

    Cleaned up logic for python processing, completed full brother names lists for js and py.

--- attendance.py
+++ attendance.py
@@ -8,7 +8,8 @@ import os
     If they are with a valid excuse, they get a 0.
     If they are totally missing (not in brother names set), they get a -1.
 """
-brothers_set = set(["Erik Pfeffer", "Jack Spoleti", "Nithin Nambi", "Sans", "Aaron"])
+
+from brother_name_set import brothers_set
 
 # Load environment variables from .env and new excel file.
 load_dotenv()
@@ -25,38 +26,32 @@ if __name__ == "__main__":
         db = get_db()
         xl = openpyxl.Workbook()
         sheet = xl.active
-        sheet.title = "Attendance_11_30_23"
+        sheet.title = "Attendance_01_26_24"
 
         attending = db["attended"]
         attendees = attending.find()
-        # attendees = sorted(attendees, key=lambda attendees: attendees["name"])
+
         missed = db["missed"]
         missees = missed.find()
-
+        # Keys are brother name, values or -1, 0, or 1 depending on attendance.
         brothers = {}
 
         # Assuming 'attendees' and 'missees' are dictionaries
         for a in attendees:
-            brothers[a["name"]] = a
+            brothers[a["name"]] = 1
 
         for m in missees:
-            brothers[m["name"]] = m
+            brothers[m["name"]] = 0
 
         for brother in brothers_set:
             if brother not in brothers.keys():
-                brothers[brother] = {"name": brother, "no_response": -1}
+                brothers[brother] = -1
 
         # Sort brother based on name
-        brothers = dict(sorted(brothers.items(), key=lambda item: item[1]["name"]))
-        for brother_name in brothers:
-            brother = brothers[brother_name]
-            # print(brother)
-            if brother.get("no_response", 0) == -1:
-                sheet.append([brother_name, -1])
-            elif brother.get("excuse", "") != "":
-                sheet.append([brother_name, 0])
-            else:
-                sheet.append([brother_name, 1])
+        brothers = dict(sorted(brothers.items()))
+
+        for brother_name, attend_value in brothers.items():
+            sheet.append([brother_name, attend_value])
 
         xl.save("ex.xlsx")
 
