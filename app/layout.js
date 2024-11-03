9e387b1483aed75edcd28406940703beb6d7785f
Erik Pfeffer <epfeffer@terpmail.umd.edu>
2024-11-03 16:52:23 -0500

    Updated for chapter and initiation+crossing 11.03.24.

--- app/layout.js
+++ app/layout.js
@@ -3,21 +3,21 @@ import "./css/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Sigma Phi Delta Chapter Attendance",
-  description: "Chapter: 10/27/24.",
-  icons: {
-    icon: "/icons/icon.ico",
-  },
-};
-
 // export const metadata = {
-//   title: "Sigma Phi Delta Initiation",
-//   description: "Initiation: 9/19/24.",
+//   title: "Sigma Phi Delta Chapter Attendance",
+//   description: "Chapter: 10/27/24.",
 //   icons: {
 //     icon: "/icons/icon.ico",
 //   },
 // };
+
+export const metadata = {
+  title: "Sigma Phi Delta Initiation",
+  description: "Chapter & Initiation/Crossing: 11/03/24.",
+  icons: {
+    icon: "/icons/icon.ico",
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
